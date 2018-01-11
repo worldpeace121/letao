dc71c17d6ca362631ee0b38c29f72b763931e3ce
dtt <1303504207@qq.com>
2018-01-11 22:23:30 +0800

    主页一半

---
+++ public/m/js/index.js
@@ -0,0 +1,41 @@
+
+$(function () {
+
+
+//公用js
+
+
+//进度条
+//ajax请求时开始，结束时完成
+//jq中提供全局监听ajax开始及结束时事件
+  $(document).ajaxStart(function () {
+    //开启进度条
+    NProgress.start();
+    
+  })
+  
+  
+  $(document).ajaxStop(function () {
+    setInterval(function () {
+      NProgress.done();
+    },1000)
+    
+  })
+  
+  
+  
+  
+  
+  
+  
+  //二级分类
+  $('.cate').on('click',function () {
+      $(this).next().slideToggle()
+  })
+
+
+  
+  
+})
+
+
