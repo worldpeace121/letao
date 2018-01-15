2198583d9ee8267d6b692e45b361035a3db8b86d
dtt <1303504207@qq.com>
2018-01-15 11:26:57 +0800

    移动端主页分类页静态页面完成

---
+++ public/pc/js/index.js
@@ -0,0 +1,15 @@
+
+$(function () {
+//  轮播图
+  var gallery = mui('.mui-slider');
+  gallery.slider({
+    interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+  });
+
+//  区域滚动
+  mui('.mui-scroll-wrapper').scroll({
+    indicators: false,
+  });
+
+
+})
